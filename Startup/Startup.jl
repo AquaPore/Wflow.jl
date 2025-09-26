921162c2af23b1167d2dcbbf8155ce7c3879444d
“AquaPore” <pollacco.water@gmail.com>
2025-09-26 21:02:30 +0100

    Solved RAM issue

--- Startup/Startup.jl
+++ Startup/Startup.jl
@@ -1,14 +1,15 @@
 using Pkg
 
-# Path="sbm_config_simple_Timoleague.toml"
+Path="sbm_config_simple_Timoleague.toml"
 # Path="sbm_config_Ballycanew.toml"
 # Path="sbm_config_Castledockrell.toml"
 # Path="sbm_config_Corduff.toml"
 # Path="sbm_config_Dunleer.toml"
-Path="sbm_config_Cregduff.toml"
+# Path="sbm_config_Cregduff.toml"
 
 Path_Home = raw"D:\JOE\MAIN\MODELS\WFLOW\Wflow.jl\Wflow"
 cd(raw"D:\JOE\MAIN\MODELS\WFLOW\Wflow.jl\Wflow")
 Pkg.activate(".")
 include(raw"D:\JOE\MAIN\MODELS\WFLOW\Wflow.jl\Wflow\src\Wflow.jl")
 Main.Wflow.run(Path)
+
