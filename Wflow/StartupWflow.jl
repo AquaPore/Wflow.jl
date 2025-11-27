2aaa348c7b094ef94ea1f552d8d01a90dc1e2257
“AquaPore” <pollacco.water@gmail.com>
2025-11-27 10:45:31 +0000

    Joe changes

--- Wflow/StartupWflow.jl
+++ Wflow/StartupWflow.jl
@@ -1,10 +1,13 @@
 # using Revise
-# instantiate
-# resolve
+using Pkg
+using Base
+
+@assert Base.VERSION == v"1.11.3"
 
 Path="sbm_config_Timoleague.toml"
 # Path="sbm_config_Moselle.toml"
+
 cd(raw"D:\JOE\MAIN\MODELS\WFLOW\Wflow.jl\Wflow")
-Pkg.activate(".")
+# Pkg.activate(".")
 include(raw"src\Wflow.jl")
 Main.Wflow.run(Path)
---
+++ Wflow/src/VISUALISATION/Visualisation.jl
@@ -0,0 +1,11 @@
+module visualisation
+"""
+    VISUALISATION()
+	 To Visualise the output streamflow
+
+	Joseph A.P. POLLACCO
+"""
+	function VISUALISATION()
+
+	end
+end
